6f8900f660cc818ac24b60e09b109f65dae99275
jeffrey <jeffreyon11@gmail.com>
2022-11-21 21:54:38 +0100

    Touches on the padding

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     }
 
     return (
-        <div className="h-screen flex justify-center items-center p-3">
+        <div className="h-screen flex justify-center items-center p-4">
             <div className="max-w-sm p-7 bg-slate-900 bg-gradient-to-b from-slate-800 to-slate-900 rounded-3xl">
                 {showing == "ratingCard" && (
                     <RatingCard
